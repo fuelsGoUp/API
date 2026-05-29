f13f4894a9c9a211d3982df48a496867df5e3090
fuelsGoUp <silvavitor12112003@gmail.com>
2026-05-29 13:01:51 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include "crow.h"
 
+int main() {
+
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/health")
+    ([](){
+
+        return "API funcionando";
+
+    });
+
+    app.port(18080)
+       .multithreaded()
+       .run();
+
+    return 0;
+}
