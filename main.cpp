23b76171f1376a1252e3152d884c1d684345e7fb
fuelsGoUp <silvavitor12112003@gmail.com>
2026-05-29 13:11:20 -0300

    Integrate ControladorTarefa into main application

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include "crow.h"
 
+#include "apresentacao/controladores/ControladorTarefa.hpp"
+
 int main() {
 
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/health")
     ([](){
-
         return "API funcionando";
-
     });
+
+    ControladorTarefa::registrarRotas(app);
 
     app.port(18080)
        .multithreaded()
