59ea4887cdd70d1d8c2c3d29912589dc0abddbae
fuelsGoUp <silvavitor12112003@gmail.com>
2026-06-02 09:19:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
 #include "crow.h"
 
+#include "infraestrutura/banco/BancoDados.hpp"
+#include "infraestrutura/repositorios/RepositorioTarefaPostgres.hpp"
+
+#include "nucleo/aplicacao/servicos/ServicoTarefa.hpp"
+
 #include "apresentacao/controladores/ControladorTarefa.hpp"
 
-int main() {
+int main()
+{
+    BancoDados banco;
+
+    RepositorioTarefaPostgres repositorio(
+        banco
+    );
+
+    ServicoTarefa servico(
+        repositorio
+    );
+
+    ControladorTarefa controlador(
+        servico
+    );
 
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/health")
-    ([](){
+    ([]()
+    {
         return "API funcionando";
     });
 
-    ControladorTarefa::registrarRotas(app);
+    controlador.registrarRotas(app);
 
     app.port(18080)
        .multithreaded()
