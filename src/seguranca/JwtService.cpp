ae7cba7a7381f3762fb2f7eb0f81b488e500e33b
fuelsGoUp <silvavitor12112003@gmail.com>
2026-06-03 20:09:06 -0300

    Add token validation method in JwtService

--- src/seguranca/JwtService.cpp
+++ src/seguranca/JwtService.cpp
@@ -47,3 +47,36 @@ std::string JwtService::gerarToken(
 
   return token;
 }
+
+bool JwtService::validarToken(
+  const std::string& token;
+)
+{
+  try
+  {
+    auto decoded = jw =
+      jwt::decode(token);
+
+    auto verifier =
+      jwt verify::verify()
+
+      .allow_algorithm(
+        jwt::algorithm::hs256{
+          segredo
+        }
+      )
+
+      with_issue(
+        "taskManager"
+      );
+
+    verifier.verify(
+      decoded
+    );
+
+    return true;
+  }
+  catch(...)
+  {
+    return false;
+  }
