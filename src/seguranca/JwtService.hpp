5703bbf979b0173c9ecc9dbc7bf9307069e086c3
fuelsGoUp <silvavitor12112003@gmail.com>
2026-06-03 19:37:23 -0300

    Add JwtService class for token generation and validation

---
+++ src/seguranca/JwtService.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+class JwtService
+{
+private:
+
+  std::string segredo;
+
+public:
+
+  JwtService();
+
+  std::string gerarToken(
+    int idUsuario;
+    const std::string& email
+  );
+
+  bool validarToken(
+    const std::string& token
+  );
+};
